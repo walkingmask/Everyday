9daaa6c4ca77ba449740e124a471ca03e890aa70
Kazuki Nagamine <walkingmask@gmail.com>
2016-05-10 09:11:51 +0900

    2016/05/10
    Done!

---
+++ AOJ/aoj_itp1_8_b.py
@@ -0,0 +1,10 @@
+# http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_8_A
+
+while 1:
+	x = raw_input()
+	if x == '0':
+		break
+	s = 0
+	for i in xrange(len(x)):
+		s += int(x[i])
+	print s
